1621c59fbe5a2c7cd6920fe1211d6f0b29e8bd8a
Ignacio Borr <145501227+IgnaBorr@users.noreply.github.com>
2026-05-28 11:16:02 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Copiar este archivo como config.js y reemplazar con tus datos de Supabase.
-// El anon key puede estar en el frontend siempre que Row Level Security esté bien configurado.
-window.SUPABASE_CONFIG = {
-  url: "https://kwuwuaqncgmyyzuksoko.supabase.co",
-  anonKey: "sb_publishable_NdEKZn8OO5ORJEjj-XkWig_bv8xD3d3"
-};
